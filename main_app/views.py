4551c6f90a4354d372c0b2b848640091762b3fd8
Your Name <you@example.com>
2025-01-13 15:15:34 -0500

    crud

--- main_app/views.py
+++ main_app/views.py
@@ -4,9 +4,24 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+# additional imports below
+from rest_framework import generics
+from .models import Cat
+from .serializers import CatSerializer
 
 # Define the home view
 class Home(APIView):
     def get(self, request):
         content = {"message": "Welcome to the cat-collector api home route!"}
         return Response(content)
+
+
+class CatList(generics.ListCreateAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+
+
+class CatDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+    lookup_field = "id"
